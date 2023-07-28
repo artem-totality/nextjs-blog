2e7b4f1ad89163237ed1ac3af1535ae37acb930e
totality <artem.totality@gmail.com>
2023-07-28 03:12:47 +0300

    Add fallback: true prerendering

--- pages/repo/[name].ts
+++ pages/repo/[name].ts
@@ -1,39 +1,39 @@
 import { useRouter } from 'next/router';
 
-// export async function getStaticPaths() {
-//   return {
-//     paths: [
-//       {
-//         params: {
-//           name: 'next.js',
-//         },
-//       }, // See the "paths" section below
-//     ],
-//     // fallback: 'blocking', // false or "blocking"
-//     fallback: true, // false or "blocking"
-//   };
-// }
-
-// export async function getStaticProps() {
-//   console.log('get static props');
-//   const res = await fetch('https://api.github.com/repos/vercel/next.js');
-//   const repo = await res.json();
-//   console.time('Time this');
-//   for (let i = 0; i < 2000000000; i++) {
-//     let s = Math.sin(i);
-//   }
-//   console.timeEnd('Time this');
-//   return { props: { repo } };
-// }
-
-export async function getServerSideProps(context) {
-  console.log(context, null, Infinity, 'ttt');
-  // console.log(context);
-  // console.log(JSON.stringify(context, null, 2));
+export async function getStaticPaths() {
   return {
-    props: { repo: { stargazers_count: 42 } },
+    paths: [
+      {
+        params: {
+          name: 'next.js',
+        },
+      }, // See the "paths" section below
+    ],
+    // fallback: 'blocking', // false or "blocking"
+    fallback: true, // false or "blocking"
   };
 }
+
+export async function getStaticProps() {
+  console.log('get static props');
+  const res = await fetch('https://api.github.com/repos/vercel/next.js');
+  const repo = await res.json();
+  console.time('Time this');
+  for (let i = 0; i < 2000000000; i++) {
+    let s = Math.sin(i);
+  }
+  console.timeEnd('Time this');
+  return { props: { repo } };
+}
+
+// export async function getServerSideProps(context) {
+//   console.log(context, null, Infinity, 'ttt');
+//   // console.log(context);
+//   // console.log(JSON.stringify(context, null, 2));
+//   return {
+//     props: { repo: { stargazers_count: 42 } },
+//   };
+// }
 
 export default function Page({ repo }) {
   const router = useRouter();
